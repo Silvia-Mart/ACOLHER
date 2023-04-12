a0d97c18e9dc89ee83dcd975b4a8e5221edf7d14
Silvia-Mart <santosmartinssilvia@gmail.com>
2023-04-12 09:55:48 -0300

    Iniciando o projeto.

---
+++ src/main/java/com/acolher/familias/FamiliasApplication.java
@@ -0,0 +1,13 @@
+package com.acolher.familias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamiliasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acolher/familias/FamiliasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acolher.familias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamiliasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
